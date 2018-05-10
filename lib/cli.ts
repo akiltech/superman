38a21ffc351c18e74af14dad37c8029bb8aac15f
Hassane Dao <dao.hassane@gmail.com>
2018-05-10 23:23:25 +0000

    update

--- lib/cli.ts
+++ lib/cli.ts
@@ -1,2 +1,10 @@
 import 'commander';
 
+var program = require('commander');
+
+program
+    .version('0.0.1')
+    .option('-l, --list [list]', 'list of customers in CSV file')
+    .parse(process.argv)
+
+console.log(program.list);
